e83ecbcd1268ba59e6e7d7592dd8a8453703e4e5
wrmay <rmay@pivotal.io>
2018-02-06 10:42:36 -0500

    saving work

--- vagrant/setup.py
+++ vagrant/setup.py
@@ -4,22 +4,12 @@ import sys
 import urllib
 
 if __name__ == '__main__':
-    if not os.path.exists('pivotal-gemfire-9.0.4.zip'):
+    gemfire_version = '9.2.2'
+    gemfire_filename = 'pivotal-gemfire-{0}.zip'.format(gemfire_version)
+
+    if not os.path.exists(gemfire_filename):
         print 'retrieving gemfire - stand by ...'
-        urllib.urlretrieve('http://download.pivotal.com.s3.amazonaws.com/gemfire/9.0.4/pivotal-gemfire-9.0.4.zip','pivotal-gemfire-9.0.4.zip')
-
-    print 'pivotal-gemfire-9.0.4.zip downloaded'
-
-    cmd = 'keytool -genkey -alias self ' + \
-        '-keystore trusted.keystore -storepass passw0rd ' + \
-        '-storetype JKS ' + \
-        '-keypass passw0rd ' + \
-        '-dname CN=trusted ' + \
-        '-validity 3650'
-
-    print cmd
-    rc = subprocess.call(cmd.split())
-    if rc == 0:
-        print 'keystore generated'
+        urllib.urlretrieve('http://download.pivotal.com.s3.amazonaws.com/gemfire/{0}/{1}'.format(gemfire_version,gemfire_filename),gemfire_filename)
+        print '{0} downloaded'.format(gemfire_filename)
     else:
-        sys.exit('keystore generation failed')
+        print '{0} is already downloaded'.format(gemfire_filename)
